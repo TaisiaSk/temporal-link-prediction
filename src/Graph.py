3b3ff3cf8320de482cb3299d334e7b9411b25869
Ermolaefff <fallen2320@yandex.ru>
2023-05-20 12:11:54 +0300

    graph edges filter by timestamp

--- src/Graph.py
+++ src/Graph.py
@@ -1,21 +1,22 @@
 class Graph(object):
-    def __init__(self, file_path : str, timestamp_col : int = 2, 
-                 number_of_lines_to_skip : int = 0, timestamp_percentile : int = 100):        
-        self.__adjacent_vertices = dict()
-        self.__edges_info = dict()
-
+    def __init__(self, file_path : str, timestamp_col : int = 2, number_of_lines_to_skip : int = 0):        
         try:
+            self.__adjacent_vertices = dict()
+            self.__edges_info = dict()
+            self.__timestamps = set()
+
             with open(file_path, "r") as file:
-                edge_id = 0
                 for _ in range(0, number_of_lines_to_skip):
                     next(file)
 
+                edge_id = 0
                 for line in file:
                     tokens = line.split()
                     v1 = int(tokens[0])
                     v2 = int(tokens[1])
                     timestamp = float(tokens[timestamp_col])
 
+                    self.__timestamps.add(timestamp)
                     self.add_edge(v1, v2, edge_id, timestamp)
                     edge_id += 1
 
@@ -23,20 +24,38 @@ class Graph(object):
             print("Could not open/read file: ", file_path)
 
 
-    def number_of_edges(self) -> int:
-        return len(self.__edges_info)
+    def number_of_edges(self, timestamp_filter : int = 100) -> int:
+        if (timestamp_filter == 100):
+            return len(self.__edges_info)
+        filter = self.__filter(timestamp_filter)
+        return len([id for id, props in self.__edges_info.items() if props[0] <= filter])
+
+
+    def max_timestamp(self) -> float:
+        return max(self.__timestamps)
+    
+
+    def min_timestamp(self) -> float:
+        return min(self.__timestamps)
 
 
     def number_of_vertices(self) -> int:
         return len(self.__adjacent_vertices)
 
 
-    def edges_ids(self) -> set:
-        return set(self.__edges_info.keys())
+    def edges_ids(self, timestamp_filter : int = 100) -> set:
+        if (timestamp_filter == 100):
+            return set(self.__edges_info.keys())
+        filter = self.__filter(timestamp_filter)
+        return set(id for id, props in self.__edges_info.items() if props[0] <= filter)
 
 
     def vertices(self) -> set:
         return set(self.__adjacent_vertices.keys())
+
+
+    def splitting_by_filter(self, timestamp_filter : int) -> float:
+        return self.number_of_edges(timestamp_filter) / self.number_of_edges()
 
 
     def get_edge_info(self, edge_id : int) -> list:
@@ -45,17 +64,28 @@ class Graph(object):
         return None
 
 
-    def adj(self, vertex_id : int) -> set:
-        if (vertex_id in self.__adjacent_vertices):
+    def adj(self, vertex_id : int, timestamp_filter : int = 100) -> set:
+        if not (vertex_id in self.__adjacent_vertices):
+            return set()
+        if (timestamp_filter == 100):
             return set(self.__adjacent_vertices[vertex_id].keys())
-        return None
+        return set(id for id in self.__adjacent_vertices[vertex_id] if self.has_edges_between(vertex_id, id, timestamp_filter))
 
 
-    def get_edges_between(self, vertex_id_1 : int, vertex_id_2 : int) -> set:
-        if (vertex_id_1 in self.__adjacent_vertices):
-            if (vertex_id_2 in self.__adjacent_vertices[vertex_id_1]):
-                return self.__adjacent_vertices[vertex_id_1][vertex_id_2]
-        return None
+    def has_edges_between(self, vertex_id_1 : int, vertex_id_2 : int, timestamp_filter : int = 100) -> bool:
+        return len(self.get_edges_between(vertex_id_1, vertex_id_2, timestamp_filter)) > 0
+
+
+    def get_edges_between(self, vertex_id_1 : int, vertex_id_2 : int, timestamp_filter : int = 100) -> set:
+        if not ((vertex_id_1 in self.__adjacent_vertices) and (vertex_id_2 in self.__adjacent_vertices[vertex_id_1])):
+            return set()
+        
+        edges = self.__adjacent_vertices[vertex_id_1][vertex_id_2]
+        if (timestamp_filter == 100):
+            return edges
+        
+        filter = self.__filter(timestamp_filter)
+        return set(id for id in edges if self.__edges_info[id][0] <= filter)
 
 
     def add_vertex(self, vertex_id : int):
@@ -125,5 +155,14 @@ class Graph(object):
         self.__adjacent_vertices[vertex_from][vertex_to].remove(edge_id)
         if (len(self.__adjacent_vertices[vertex_from][vertex_to]) == 0):
             del self.__adjacent_vertices[vertex_from][vertex_to]
+
+
+    def __filter(self, timestamp_filter : int):
+        if (timestamp_filter < 0 or timestamp_filter > 100):
+            raise Exception(f"Required filter value is out of range: ", timestamp_filter)
+        
+        max = self.max_timestamp()
+        min = self.min_timestamp()
+        return (max - min) * timestamp_filter / 100 + min
 
         
