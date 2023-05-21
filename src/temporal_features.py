7736a61d98b60c09ee320cf7220e1e809a004f5a
TaisiaSk <71456151+TaisiaSk@users.noreply.github.com>
2023-05-21 18:30:01 +0300

    some fix

--- src/temporal_features.py
+++ src/temporal_features.py
@@ -1,8 +1,27 @@
 from math import exp, sqrt, log
 import numpy as np
 from typing import Callable
+from functools import lru_cache
+import time
 
-from Graph import Graph
+
+from graph import Graph
+
+# buffer = {f'{i}{j}': {} for i in range(3) for j in range(9)}
+
+# def __get_buffer_key0(time_strategy: Callable[[float, float, float], float], \
+#                       agg_strategy: Callable[[list], float] | None = None) -> str:
+#     i = get_weightings().index(time_strategy)
+#     j = 8
+    
+#     if (agg_strategy != None):
+#         j = get_aggregations().index(agg_strategy)
+
+#     return f'{i}{j}'
+
+# def __get_buffer_key1(u: int, v: int) -> str:
+#     return f'{min(u, v)}-{max(u, v)}'
+    
 
 # Temporal topological features
 
@@ -27,29 +46,35 @@ def sqrt_w(t: float, t_min: float, t_max: float) -> float:
 
 
 # All weighting strategies
-def get_weightings() -> dict:
-    strategies = {'lin': lin_w, \
-                  'exp': exp_w, \
-                  'sqrt': sqrt_w}
-    
-    return strategies
+def get_weightings() -> tuple:
+    return (lin_w, exp_w, sqrt_w)
 
+
+# def __q1(x):
+#     return np.quantile(x, 0.25)
+
+# def __q4(x):
+#     return np.quantile(x, 0.75)
 
 # All past event aggregation strategies
-def get_aggregations() -> dict:
-    strategies = {'q0': min, \
-                  'q1': lambda x: np.quantile(x, 0.25), \
-                  'q2': np.median, \
-                  'q3': lambda x: np.quantile(x, 0.75), \
-                  'q4': max, \
-                  'sum': sum, \
-                  'mean': np.mean, \
-                  'variance': np.var}
+def get_aggregations() -> tuple:
+    # q0, q1, q2, q3, q4, sum, mean, variance
+    strategies = (min, \
+                  lambda x: np.quantile(x, 0.25), \
+                #   __q1, \
+                  np.median, \
+                  lambda x: np.quantile(x, 0.75), \
+                #   __q4, \
+                  max, \
+                  sum, \
+                  np.mean, \
+                  np.var)
     
     return strategies
 
 
 # The edge weight depending on the strategies
+@lru_cache
 def get_weight(u: int, \
                v: int, \
                graph: Graph, \
@@ -58,12 +83,15 @@ def get_weight(u: int, \
                agg_strategy: Callable[[list], float] | None, \
                is_multiedge: bool = False) -> float:
     
+    # key0 = __get_buffer_key0(time_strategy, agg_strategy)
+    # key1 = __get_buffer_key1(u, v)
+
+    # if (key1 in buffer[key0]):
+    #     return buffer[key0][key1]
+
     edges = graph.get_edges_between(u, v, timestamp)
     t_min = graph.min_timestamp()
     t_max = graph.max_timestamp()
-
-    if (len(edges) < 1):
-        return 0
 
     if (is_multiedge):
         timestamps = []
@@ -72,13 +100,17 @@ def get_weight(u: int, \
             timestamps.append(graph.get_edge_info(edgeId)[0])
 
         weightings = [time_strategy(t, t_min, t_max) for t in timestamps]
+        weight = agg_strategy(weightings)
+        # buffer[key0][key1] = weight
 
-        return agg_strategy(weightings)
+        return weight
     
     edgeId = edges.pop()
     t = graph.get_edge_info(edgeId)[0]
+    weight = time_strategy(t, t_min, t_max)
+    # buffer[key0][key1] = weight
 
-    return time_strategy(t, t_min, t_max)
+    return weight
 
     
 # Weighted topological features
@@ -109,6 +141,7 @@ def _aa(u: int, \
         aa += (weight_to_u + weight_to_v) / (log(1 + curr_sum))
 
     return aa
+
 
 # Common Neighbours (CN)
 def _cn(u: int, \
@@ -154,6 +187,9 @@ def _jc(u: int, \
     for neighbor in neighbors_v:
         sum_v += get_weight(neighbor, v, graph, timestamp, time_strategy, agg_strategy, is_multiedge)
 
+    if (sum_u == 0 and sum_v == 0):
+        return 0
+
     for neighbor in common_neighbors:
         weight_to_u = get_weight(neighbor, u, graph, timestamp, time_strategy, agg_strategy, is_multiedge)
         weight_to_v = get_weight(neighbor, v, graph, timestamp, time_strategy, agg_strategy, is_multiedge)
@@ -190,23 +226,33 @@ def _pa(u: int, \
 # Get final topological features
 def get_topological_feature(u: int, v: int, graph: Graph, timestamp: int) -> list:
     result = []
-    features = {'AA': _aa, 'CN': _cn, 'JC': _jc, 'PA': _pa}
+    features = (_aa, _cn, _jc, _pa)
     
-    # is_multigraph = graph.is_multigraph()
-    is_multigraph = True
+    is_multigraph = graph.is_multigraph()
     time_strategies = get_weightings()
 
     if (is_multigraph):
         agg_strategies = get_aggregations()
 
-        for feature in features.values():
-            for weight in time_strategies.values():
-                for agg in agg_strategies.values():
+        for feature in features:
+            for weight in time_strategies:
+                for agg in agg_strategies:
                     result.append(feature(u, v, graph, timestamp, weight, agg, is_multiedge=True))
 
     else:  
-        for feature in features.values():  
-            for weight in time_strategies.values():
+        for feature in features:  
+            for weight in time_strategies:
                 result.append(feature(u, v, graph, timestamp, weight, is_multiedge=False))
 
     return result
+
+
+# a = Graph(file_path='./data/opsahl-ucsocial.tsv', timestamp_col=3, number_of_lines_to_skip=2)
+
+# start_time = time.time()
+# features = get_topological_feature(105, 8, a, 100)
+# print("--- %s seconds ---" % (time.time() - start_time))
+
+# print(a.is_multigraph())
+# print(features)
+# print(len(features))
