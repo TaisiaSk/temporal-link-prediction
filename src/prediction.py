2949e0417ada2a647fcff2848f8c980cfd8ae6df
TaisiaSk <71456151+TaisiaSk@users.noreply.github.com>
2023-05-23 16:59:23 +0300

    added data_name arg to predict func

--- src/prediction.py
+++ src/prediction.py
@@ -9,21 +9,21 @@ import matplotlib.pyplot as plt
 from features_collector import features_to_matrix
 
 
-def prediction(data: tuple) -> float:
+def prediction(data: tuple, data_name: str) -> float:
     y, X = data
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
-    logreg = LogisticRegression(max_iter=10000,  n_jobs=-1, random_state=42)
-    logreg.fit(X_train, y_train)
-
-    pred = logreg.predict_proba(X_test)[:,1]
+    # # Variant without standartization
+    # logreg = LogisticRegression(max_iter=10000,  n_jobs=-1, random_state=42)
+    # logreg.fit(X_train, y_train)
+    # pred = logreg.predict_proba(X_test)[:,1]
 
     # Variant with standartization data
-    # pipe = make_pipeline(StandardScaler(), 
-    #                      LogisticRegression(max_iter=10000,  n_jobs=-1, random_state=42))
-    # pipe.fit(X_train, y_train)
-    # pred = pipe.predict_proba(X_test)[:,1]
+    pipe = make_pipeline(StandardScaler(), 
+                         LogisticRegression(max_iter=10000,  n_jobs=-1, random_state=42))
+    pipe.fit(X_train, y_train)
+    pred = pipe.predict_proba(X_test)[:,1]
 
     auc = roc_auc_score(y_test, pred)
     fpr, tpr, thresholds = roc_curve(y_test, pred)
@@ -36,13 +36,11 @@ def prediction(data: tuple) -> float:
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Probability')
     plt.ylabel('True Positive Probability')
-
-    # Сhange the title to the dataset name
-    plt.title('Receiver operating characteristic (ROC)')
+    plt.title(f'{data_name.capitalize()} dataset')
     plt.legend(loc="lower right")
 
-    # Uncomment to save plot, change file name
-    # plt.savefig('ucsocial.png')
+    # Uncomment to save plot
+    # plt.savefig(f'{data_name}.png')
 
     plt.show()
 
@@ -54,6 +52,6 @@ def prediction(data: tuple) -> float:
 
 # Testing
 
-# dataset = {'file_name' : 'ucsocial.tsv', 'timestamp_col' : 3, 'number_of_lines_to_skip' : 2, 'filter' : 18}
+# dataset = {'file_name' : 'email.tsv', 'timestamp_col' : 3, 'number_of_lines_to_skip' : 1, 'filter' : 26}
 # data = features_to_matrix(dataset)
-# print(prediction(data))
+# print(prediction(data, dataset['file_name'].split('.')[0]))
