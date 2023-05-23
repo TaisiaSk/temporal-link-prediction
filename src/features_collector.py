bddd7ea55ee4f4535b52c8523b1a5a3de8eca53f
Ermolaefff <fallen2320@yandex.ru>
2023-05-23 18:28:57 +0300

    static features

--- src/features_collector.py
+++ src/features_collector.py
@@ -1,11 +1,17 @@
 from graph import Graph
 from logger import Logger
 from main import datasets
-from temporal_features import get_temporal_features as get_features
+from temporal_features import get_temporal_features as get_temporal
+from static_features import get_static_properties as get_static
 import numpy as np
 
-def features_to_matrix(dataset : dict) -> tuple:
-    features_logger = Logger(dir='../features/', logs_file_name=dataset['file_name'] + '.json', dump_before_del=False)
+def features_to_matrix(dataset : dict, static : bool) -> tuple:
+    if (static):
+        subdir = 'static/'
+    else:
+        subdir = 'temporal/'
+
+    features_logger = Logger(dir='../features/' + subdir, logs_file_name=dataset['file_name'] + '.json', dump_before_del=False)
     features = features_logger.get_features()
 
     vector = []
@@ -18,7 +24,12 @@ def features_to_matrix(dataset : dict) -> tuple:
     return np.array(vector), np.array(matrix)
 
 
-def find_features(current_dataset : dict):
+def find_features(current_dataset : dict, static : bool):
+    if (static):
+        subdir = 'static/'
+    else:
+        subdir = 'temporal/'
+
     file_path = '../data/' + current_dataset['file_name']
     timestamp_col = current_dataset['timestamp_col']
     number_of_lines_to_skip = current_dataset['number_of_lines_to_skip']
@@ -26,7 +37,7 @@ def find_features(current_dataset : dict):
 
     graph = Graph(file_path, timestamp_col, number_of_lines_to_skip, filter)
     pairs_logger = Logger(dir='../pairs/', logs_file_name=current_dataset['file_name'] + '.json', dump_before_del=False)
-    features_logger = Logger(dir='../features/', logs_file_name=current_dataset['file_name'] + '.json', saving_step=30)
+    features_logger = Logger(dir='../features/' + subdir, logs_file_name=current_dataset['file_name'] + '.json', saving_step=1000)
 
     pairs = pairs_logger.get_pairs()
     max_amount = min(len([pair for pair in pairs if pair[2] == 0]), len([pair for pair in pairs if pair[2] == 1]))
@@ -41,15 +52,30 @@ def find_features(current_dataset : dict):
         if (counter[appearance] == max_amount) or (features_logger.contains(v1, v2)):
             continue
         print(v1, v2, counter)
-        features = [appearance] + get_features(v1, v2, graph).tolist()
+        if (static):
+            features = [appearance] + get_static(v1, v2, graph)
+        else:
+            features = [appearance] + get_temporal(v1, v2, graph).tolist()
+       
         features_logger.log(v1, v2, features)
         counter[appearance] += 1
 
     features_logger.dump()
 
 
-# current_dataset = datasets[0] 
-# find_features(current_dataset)
+for current_dataset in datasets[0 : 1]:
+    print(current_dataset['file_name'])
+    file_path = '../data/' + current_dataset['file_name']
+    timestamp_col = current_dataset['timestamp_col']
+    number_of_lines_to_skip = current_dataset['number_of_lines_to_skip']
+    filter = current_dataset['filter']
+
+    g = Graph(file_path, timestamp_col, number_of_lines_to_skip, filter)
+    v1 = 217259
+    v2 = 235506
+    print(get_temporal(v1, v2, g))
+
+#  find_features(current_dataset, static=True)
 # print(len(vector), len(matrix))
 # print(len(matrix[0]))
         
