05102425451e681f72e728b523986297970f3094
TaisiaSk <71456151+TaisiaSk@users.noreply.github.com>
2023-05-25 15:35:44 +0300

    add figures for all datasets

--- src/config.py
+++ src/config.py
@@ -1,4 +1,8 @@
 datasets = [{'file_name' : 'bitcoinotc.tsv', 'weight_col' : 2, 'timestamp_col' : 3, 'number_of_lines_to_skip' : 1, 'filter' : 54, 'is_multigraph' : True}, 
             {'file_name' : 'bitcoinalpha.tsv', 'weight_col' : 2, 'timestamp_col' : 3, 'number_of_lines_to_skip' : 1, 'filter' : 48, 'is_multigraph' : True}, 
             {'file_name' : 'email.tsv', 'weight_col' : 2, 'timestamp_col' : 3, 'number_of_lines_to_skip' : 1, 'filter' : 70, 'is_multigraph' : False},
-            {'file_name' : 'digg.tsv', 'weight_col' : 2, 'timestamp_col' : 3, 'number_of_lines_to_skip' : 1, 'filter' : 99, 'is_multigraph' : True}]
+            {'file_name' : 'digg.tsv', 'weight_col' : 2, 'timestamp_col' : 3, 'number_of_lines_to_skip' : 1, 'filter' : 99, 'is_multigraph' : True}, 
+            {'file_name' : 'askubuntu.tsv', 'weight_col' : 2, 'timestamp_col' : 3, 'number_of_lines_to_skip' : 1, 'filter' : 84, 'is_multigraph' : False}, 
+            {'file_name' : 'loans.tsv', 'weight_col' : 2, 'timestamp_col' : 3, 'number_of_lines_to_skip' : 1, 'filter' : 75, 'is_multigraph' : True}, 
+            {'file_name' : 'superuser.tsv', 'weight_col' : 2, 'timestamp_col' : 3, 'number_of_lines_to_skip' : 1, 'filter' : 77, 'is_multigraph' : False}, 
+            {'file_name' : 'mathoverflow.tsv', 'weight_col' : 2, 'timestamp_col' : 3, 'number_of_lines_to_skip' : 1, 'filter' : 65, 'is_multigraph' : False}]
